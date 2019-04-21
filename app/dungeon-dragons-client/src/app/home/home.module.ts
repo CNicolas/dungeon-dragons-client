2f5547a83b7adf108934fff14d1994c7baf29bc0
CNicolas <clement92nicolas@gmail.com>
2019-04-21 10:04:05 +0200

    feat(home): Improve style

--- app/dungeon-dragons-client/src/app/home/home.module.ts
+++ app/dungeon-dragons-client/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import {HomeComponent} from './home.component'
 import {BrowserModule} from '@angular/platform-browser'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {ReactiveFormsModule} from '@angular/forms'
-import {MatButtonModule, MatIconModule, MatInputModule} from '@angular/material'
+import {MatButtonModule, MatIconModule, MatInputModule, MatListModule} from '@angular/material'
 import {NgxsModule} from '@ngxs/store'
 import {PlayerState} from '../shared/store'
+import {FlexLayoutModule} from '@angular/flex-layout'
 
 @NgModule({
   declarations: [
@@ -15,11 +16,13 @@ import {PlayerState} from '../shared/store'
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     NgxsModule.forFeature([PlayerState])
-  ],
+  ]
 })
 export class HomeModule {
 }
